b64e6fd289fdbd99a55f8dc7f3837e7784e8d1a6
Kenan Casey <kenancasey@gmail.com>
2017-01-19 10:47:43 -0600

    initial commit

--- CIS268-Intro-Demo/Program.cs
+++ CIS268-Intro-Demo/Program.cs
@@ -10,6 +10,18 @@ namespace CIS268_Intro_Demo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your name. ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age in years. ");
+            int age = int.Parse( Console.ReadLine() );
+
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("You are {0} months old.", age*12 );
+
+            Console.ReadKey();
+
         }
     }
 }
